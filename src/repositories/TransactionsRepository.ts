2c4666cad9f29dc924b856fbb2712ae3b249ff5a
Adelmo Dias <adelmodias@outlook.com>
2020-09-08 20:29:46 -0300

    Create transaction working

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,37 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
+}
+
+interface TransactionDTO {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+    private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+    constructor() {
+        this.transactions = [];
+    }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+    public all(): Transaction[] {
+        return this.transactions;
+    }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+    // public getBalance(): Balance {}
 
-  public create(): Transaction {
-    // TODO
-  }
+    public create({ title, value, type }: TransactionDTO): Transaction {
+        const transaction = new Transaction({ title, value, type });
+
+        this.transactions.push(transaction);
+
+        return transaction;
+    }
 }
 
 export default TransactionsRepository;
